61243044f2330cd3c8014662b6355aa5b59bb23a
Jack Cole <jackdcole@gmail.com>
2020-04-23 22:27:24 -0400

    Added Runge Kutta - Jack

--- main.m
+++ main.m
@@ -67,28 +67,26 @@ function f = contraints(p_0)
 
 global a e omega w v i eta P c mu
 
-[t,xp] = ode45(@sys,[0 t1], [x0;p_0]);
+[xp] = RungeKutta(sys,[0 t1], [x0;p_0]);
 xx = xp(end,:);
-f(1) = xx(2) - h;
-f(2) = xx(4);
 
-p = a*(1 - e^2);
-f = e*cos(w + omega);
-g = e*sin(w + omega);
-h = tan(i/2)*cos(omega);
-k = tan(i/2)*sin(omega);
-L = omega + w + v;
-
-T = (2*eta*P)/c;
-
-w = 1 + f*cos(L) + g*sin(L);
-s = sqrt(1 + h^2 + k^2);
+f(1) = p - a*(1 - e^2);
+f(2) = f - e*cos(w + omega);
+f(3) = g - e*sin(w + omega);
+f(4) = h - tan(i/2)*cos(omega);
+f(5) = k - tan(i/2)*sin(omega);
+f(6) = L - omega + w + v;
+f(7) = T - (2*eta*P)/c;
+f(8) = w - 1 + f*cos(L) + g*sin(L);
+f(9) = s - sqrt(1 + h^2 + k^2);
 
 end
 
 function [xdot, mdot] = sys(t,x)
 
 global mu
+
+
 
 alpha_vec = [sin(alpha)*cos(beta) cos(alpha)*cos(beta) sin(beta)]';
 
@@ -105,5 +103,23 @@ xdot = M*(T/m)*alpha_vec + D;
 mdot = -T/c;
 lam_xdot = -(lam'*dMdx*(T/m)*alpha_vec + lam'*dDdx);
 lam_mdot = -norm(lam'*M)*(T/m^2);
+
+end
+
+function [y] = RungeKutta(func, tspan, y0)
+
+h=1;                                               % step size
+x = 0:h:5;                                         % Calculates upto y(3)
+y = zeros(1,length(x)); 
+y(1) = y0;                                         % initial condition
+
+for i = tspan(1):tspan(2)                          % calculation loop
+    k_1 = func(x(i),y(i));
+    k_2 = func(x(i)+0.5*h,y(i)+0.5*h*k_1);
+    k_3 = func((x(i)+0.5*h),(y(i)+0.5*h*k_2));
+    k_4 = func((x(i)+h),(y(i)+k_3*h));
+
+    y(i+1) = y(i) + (1/6)*(k_1+2*k_2+2*k_3+k_4)*h;  % main equation
+end
 
 end
